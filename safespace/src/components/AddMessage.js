1ce1633b21fa7ef1bb4df70a73e352725ec36a77
ayunas <ayunas@gmail.com>
2019-05-04 11:25:07 -0500

    wrote duration() method to calculate duration based off of input of the user and delayed the firing of the sms post request to the Twilio API.  tested with a 1 minute duration.  funcitoning correctly.

--- safespace/src/components/AddMessage.js
+++ safespace/src/components/AddMessage.js
@@ -10,12 +10,7 @@ class AddMessage extends React.Component {
         }
     }
 
-    msgMaker = (e) => {
-        e.preventDefault();
-        console.log(e.target[1].value, 'time in msgMaker');
-        console.dir(e.target);
-
-        const phone = e.target[1].value;
+    duration = (e) => {
         const targetTime = e.target[2].value;
         const currentHours = new Date().getHours();
         const currentMinutes = new Date().getMinutes();
@@ -26,18 +21,37 @@ class AddMessage extends React.Component {
         const durationMinutes = targetMinutes - currentMinutes;
         const duration = durationHours * 60 * 60 * 1000 + durationMinutes * 60 * 1000
         console.log(currentHours,currentMinutes,targetHours,targetMinutes,'duration: ',duration);
+        return duration;
+    }
+
+
+    msgMaker = (e) => {
+        e.preventDefault();
+        console.log(e.target[1].value, 'time in msgMaker');
+        console.dir(e.target);
+        const durationTime = this.duration(e);
+
+        const phone = e.target[1].value;
+        // const targetTime = e.target[2].value;
+        // const currentHours = new Date().getHours();
+        // const currentMinutes = new Date().getMinutes();
+        // const pieces = targetTime.split(':');
+        // const targetHours = parseInt(pieces[0],10);
+        // const targetMinutes = parseInt(pieces[1],10);
+        // const durationHours = targetHours - currentHours;
+        // const durationMinutes = targetMinutes - currentMinutes;
+        // const duration = durationHours * 60 * 60 * 1000 + durationMinutes * 60 * 1000
+        // console.log(currentHours,currentMinutes,targetHours,targetMinutes,'duration: ',duration);
 
         const message = {
             body : e.target[0].value,
-            // scheduled : e.target[1].value
             scheduled : new Date()
         }
 
-        this.props.add(message,phone,duration);
+        this.props.add(message,phone,durationTime);
     }
 
     render() {
-
         return (
                 <form className='add-msg' onSubmit={ (e) => this.msgMaker(e)}>
                     <textarea id='message' rows='10' cols='30' placeholder='Enter Inspirational Message Here'/><br/>
--- safespace/src/components/SendSMS.js
+++ safespace/src/components/SendSMS.js
@@ -16,9 +16,29 @@ export default class SendSMS extends Component {
         })
     }
 
-  render() {
+      duration = (e,time) => {
+        const currentHours = new Date().getHours();
+        const currentMinutes = new Date().getMinutes();
+        const pieces = time.split(':');
+        const targetHours = parseInt(pieces[0],10);
+        const targetMinutes = parseInt(pieces[1],10);
+        const durationHours = targetHours - currentHours;
+        const durationMinutes = targetMinutes - currentMinutes;
+        const duration = durationHours * 60 * 60 * 1000 + durationMinutes * 60 * 1000
+        return duration;
+      }
+
+    render() {
+   
     return (
-      <form onSubmit={(e) => {e.preventDefault(); this.props.smsToggle(); this.props.sms(this.props.quote,'+14694143109',this.state.phone)}}>
+      <form onSubmit={ (e) => {
+        e.preventDefault(); 
+        this.props.smsToggle(); 
+        const duration = this.duration(e,this.state.time);
+        console.log(duration, 'duration returned from duration()');
+        setTimeout(this.props.sms,duration, this.props.quote,'+14694143109',this.state.phone);
+        // this.props.sms(this.props.quote,'+14694143109',this.state.phone);
+        }}>
         <input type='time' name='time' value={this.state.time} onChange={this.handleChange}/>
         <input type='text' name='phone' value={this.state.phone} onChange={this.handleChange} required />
         <button>Schedule</button>
